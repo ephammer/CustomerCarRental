391f1e273d50d0d8ee0ab32b61b12e82061736f8
ephammer <kidon.613@gmail.com>
2018-01-16 14:11:14 +0200

    work on progress

---
+++ app/src/main/java/com/ephraimhammer/jct/customercarrental/CarsBanches.java
@@ -0,0 +1,21 @@
+package com.ephraimhammer.jct.customercarrental;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CarsBanches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cars_banches);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
